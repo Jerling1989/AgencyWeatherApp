c895f4c84bd9b920a118c8456f357ae9497b40cd
Jacob <jerling1989@gmail.com>
2018-06-24 14:36:59 -0400

    rednering a list of cities

--- src/components/city_index.js
+++ src/components/city_index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchCities } from '../actions';
@@ -7,13 +8,30 @@ class CityIndex extends Component {
 		this.props.fetchCities();
 	}
 
+	renderCities() {
+		return _.map(this.props.cities, post => {
+			return (
+				<li className="list-group-item" key={post.id}>
+					{post.title}
+				</li>
+			);
+		});
+	}
+
 	render() {
 		return (
 			<div>
-				City Index
+				<h3>Cities</h3>
+				<ul className="list-group">
+					{this.renderCities()}
+				</ul>
 			</div>
 		);
 	}
 }
 
-export default connect(null, { fetchCities })(CityIndex);
+function mapStateToProps(state) {
+	return { cities: state.cities };
+}
+
+export default connect(mapStateToProps, { fetchCities })(CityIndex);
