3bb5ddf42cd74ec1e8f33ff8576319c663d8322f
Jacob <jerling1989@gmail.com>
2018-06-24 14:07:48 -0400

    set up city reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import CityReducer from './reducer_cities';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  cities: CityReducer
 });
 
 export default rootReducer;
