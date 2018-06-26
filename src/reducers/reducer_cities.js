03e5b983cd8744f2889e83a8629475b68d7fbedc
Jacob <jerling1989@gmail.com>
2018-06-26 09:04:25 -0400

    omit city data

--- src/reducers/reducer_cities.js
+++ src/reducers/reducer_cities.js
@@ -1,8 +1,12 @@
 import _ from 'lodash';
-import { FETCH_CITIES, CITY_WEATHER } from '../actions';
+import { FETCH_CITIES, CITY_WEATHER, DELETE_CITY } from '../actions';
 
 export default function(state = {}, action) {
 	switch (action.type) {
+
+		case DELETE_CITY:
+			return _.omit(state, action.payload);
+
 		case CITY_WEATHER:
 			return { ...state, [action.payload.data.id]: action.payload.data };
 
