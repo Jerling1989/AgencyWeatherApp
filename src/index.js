ad82b4e413b3662c7ef1be5409849c87649a7bf3
Jacob <jerling1989@gmail.com>
2018-06-27 05:17:17 -0400

    index.js comments

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+// IMPORT REACT/REDUX DEPENDENCIES
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
-
+// IMPORT COMPONENTS AND REDUCERS
 import reducers from './reducers';
 import Header from './components/header';
 import CityIndex from './components/city_index';
@@ -13,6 +14,7 @@ import CityWeather from './components/city_weather';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
+// RENDER REACT COMPONENTS IN THE DOM
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
