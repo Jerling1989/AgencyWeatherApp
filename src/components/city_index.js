6a67daed236d77fd67e7a6cdd5fd6ee4e6fd6da8
Jacob <jerling1989@gmail.com>
2018-06-27 05:26:18 -0400

    city_index comments

--- src/components/city_index.js
+++ src/components/city_index.js
@@ -1,19 +1,25 @@
-import _ from 'lodash';
+// IMPORT REACT/REDUX DEPENDENCIES
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+// IMPORT LODASH
+import _ from 'lodash';
+// IMPORT ACTION CREATOR/COMPONENT
 import { fetchCities } from '../actions';
-
 import CityImage from './city_image';
 
+// CREATE CITYINDEX COMPONENT
 class CityIndex extends Component {
 	componentDidMount() {
+		// GET LIST OF CITIES FROM DATABASE
 		this.props.fetchCities();
 	}
 
+	// RENDER CITIES HELPER FUNCTION
 	renderCities() {
 		return _.map(this.props.cities, city => {
 			return (
+				{/* CREATE LINKED CARD DIV FOR EACH CITY */}
 				<li className="col s12 m6" key={city.id}>
 					<Link id="back-link" to={`/city/${city.id}`}>
 			      <CityImage name={city.title} />
@@ -22,13 +28,14 @@ class CityIndex extends Component {
 			);
 		}).reverse();
 	}
-
+	// RENDER COMPONENT
 	render() {
 		return (
 			<div>
-				<div className="">
+				<div>
+					{/* ADD NEW CITY BUTTON */}
 					<Link className="btn-floating btn-large waves-effect waves-light teal" to="/city/new">
-						<i className="material-icons">add</i> {/*Add a City*/}
+						<i className="material-icons">add</i>
 					</Link>
 				</div>
 				<h3 className="center-align">Your Cities</h3>
@@ -44,4 +51,5 @@ function mapStateToProps(state) {
 	return { cities: state.cities };
 }
 
+// EXPORT COMPONENT
 export default connect(mapStateToProps, { fetchCities })(CityIndex);
