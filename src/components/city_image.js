15718cdaf904bc4e5fe65c7a91af6f1ffb826ad6
Jacob <jerling1989@gmail.com>
2018-06-27 05:21:34 -0400

    city_image comments

--- src/components/city_image.js
+++ src/components/city_image.js
@@ -5,27 +5,31 @@ import axios from 'axios';
 
 // CREATE GOOGLE MAP CLASS COMPONENT
 class CityImage extends Component {
-
 	componentWillMount() {
+		// REPLACE SPACES WITH HYPHENS AND LOWERCASE STR THEN SEARCH FOR IMG
 		const search = this.props.name.replace(/[^\w]/g,'-').toLowerCase();
 		axios.get(`https://api.teleport.org/api/urban_areas/slug:${search}/images/`)
 			.then((res) => {
+				// ADD IMG SEARCH RES TO STATE
 				this.setState({ cityImage: res.data.photos[0].image.web })
 			});
 	}
 
+	// RENDER COMPONENT
 	render() {
+		// IF THERE IS NO DATA YET DISPLAY LOADING MSG
 		if(!this.state) {
 			return <div>Loading...</div>
 		}
 
 		return(
-			
 			<div className="card z-depth-3 city-index">
 				<div class="card-image waves-effect waves-block waves-light">
+					{/* CITY IMAGE TAG */}
 		      <img class="activator" src={this.state.cityImage} />
 		    </div>
         <div className="card-content white-text">
+        	{/* CITY NAME */}
           <span className="center-align card-title">{this.props.name}</span>
         </div>
       </div>
@@ -33,6 +37,5 @@ class CityImage extends Component {
 	}
 }
 
-
-// EXPORT GOOGLEMAP COMPONENT
+// EXPORT COMPONENT
 export default CityImage;
